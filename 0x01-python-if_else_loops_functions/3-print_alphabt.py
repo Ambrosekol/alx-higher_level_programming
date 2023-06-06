b8a9fe0b4ab297d5ce00ad9e98bec1eb57713a2a
ambrose <kolus7381@gmail.com>
2023-06-06 22:50:12 +0100

    fixed EOF

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,4 +2,4 @@
 for alph in range(ord('a'), ord('z') + 1):
     if alph == ord('q') or alph == ord('e'):
         continue
-    print(f"{chr(alph)}", end="")
+    print("{}".format(chr(alph)), end="")
