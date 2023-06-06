d59a416fdab7d40f88db3322cd676b1fc04b7d3f
ambrose <kolus7381@gmail.com>
2023-06-06 23:19:18 +0100

    correction

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    for char in range(ord('a', ord('z') + 1)):
+    for char in range(ord('a'), ord('z') + 1):
         if ord(c) == char:
             return True
         else:
