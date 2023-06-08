d936b9a92afbdb82a3571f8ac023c7bfc92432bf
ambrose <kolus7381@gmail.com>
2023-06-08 22:41:40 +0100

    new commit

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import importlib
-    hidden_module = importlib.import_module("hidden_4")
-    names = [name for name in dir(hidden_module)
+    mod = importlib.import_module("hidden_4")
+    names = [name for name in dir(mod)
              if not name.startswith("__")]
     for name in sorted(names):
         print(name)
