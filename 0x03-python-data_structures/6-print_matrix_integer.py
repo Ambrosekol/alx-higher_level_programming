9d818a47b3db1cf3ecf009c10adede33c2e15176
ambrose <kolus7381@gmail.com>
2023-06-10 17:05:57 +0100

    new commit

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,9 @@
 
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for num in row:
-            print("{:d}".format(num), end=" ")
+        indx = len(row) - 1
+        while indx >= 0:
+            print("{:d}".format(row[indx]), end="")
+            if indx != 0:
+                print(" ")
         print()
