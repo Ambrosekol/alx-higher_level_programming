10d493b992b803f7f1daa8f4a14f7e405262e489
ambrose <kolus7381@gmail.com>
2023-06-11 16:50:28 +0100

    fixed issues

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if len(sentence) == 0:
-        return ("None", 0)
+        return reversed(("None", 0))
     else:
         w_len = len(sentence)
         return (w_len, sentence[0])
