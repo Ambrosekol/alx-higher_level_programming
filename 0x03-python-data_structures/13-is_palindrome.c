ba24fe87eb9b2ff9eedaecdd1ece40209d826490
ambrose <kolus7381@gmail.com>
2023-06-12 12:27:02 +0100

    new commit

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,43 +1,39 @@
 #include "lists.h"
 /**
   * is_palindrome - Checks if a list is a palendrome
-  * head: List Head
+  * @head: List Head
   * Return: returns a 1 or 0
   */
 
 int is_palindrome(listint_t **head)
 {
-    listint_t *arry;
-    int buffer[1024], indx, fwd, prev;
+	listint_t *arry;
+	int buffer[1024], indx, fwd, prev;
 
-    arry = *head;
-    indx = 0;
-    fwd = 0;
-    if (*head == NULL)
-        return (1);
-    while (arry != NULL)
-    {
-        buffer[indx] = arry->n
-        indx += 1;
-        arry = arry->next;
-    }
-    indx += 1;
-    if ((indx % 2) != 0)
-    {
-        return (0);
-    } 
-    prev = indx - 1;
-    while (fwd < (indx / 2))
-    {
-        while (prev > fwd)
-        {
-            if (buffer[fwd] != buffer[prev])
-            {
-                return (0);
-            }
-            prev -= 1;
-        }
-        fwd += 1;
-    }
-    return (1);
+	arry = *head;
+	indx = 0;
+	fwd = 0;
+	if (*head == NULL)
+		return (1);
+	while (arry != NULL)
+	{
+		buffer[indx] = arry->n;
+		indx += 1;
+		arry = arry->next;
+	}
+	if ((indx % 2) != 0)
+	{
+		return (0);
+	}
+	prev = indx - 1;
+	while (fwd < (indx / 2))
+	{
+		if (buffer[fwd] != buffer[prev])
+		{
+			return (0);
+		}
+		prev -= 1;
+		fwd += 1;
+	}
+	return (1);
 }
