8d7af4730a0e79865233a14c342d540473eeb989
ambrose <kolus7381@gmail.com>
2023-06-12 12:49:47 +0100

    new commit

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+    if my_list is None:
+        my_list = []
+
     length = len(my_list)
     newlst = [my_list[val] for val
               in range((length - 1), -1, -1)]
