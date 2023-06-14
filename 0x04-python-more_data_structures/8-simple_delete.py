d71c9aa1e192d0a18bdf94e5081a1c679e2f0339
ambrose <kolus7381@gmail.com>
2023-06-14 18:58:51 +0100

    nwe commit

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
 def simple_delete(a_dictionary, key=""):
-    del a_dictionary[key]
+    for elem in list(a_dictionary):
+        if key == elem:
+            del a_dictionary[key]
     return a_dictionary
