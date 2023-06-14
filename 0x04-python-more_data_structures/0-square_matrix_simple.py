26306efa092a9cc275b70e91d5119b6128509b83
ambrose <kolus7381@gmail.com>
2023-06-14 15:30:10 +0100

    new commit

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,5 +3,5 @@ def square_matrix_simple(matrix=[]):
     newlist = list()
     for row in matrix:
         newlist.append(list(map(
-            lambda x: x **2, row)))
+            lambda x: x**2, row)))
     return newlist
