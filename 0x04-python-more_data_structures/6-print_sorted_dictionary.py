a633ce374ecf74db7c151785fc6e183db4610487
ambrose <kolus7381@gmail.com>
2023-06-14 17:45:32 +0100

    new commit

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,4 +3,4 @@
 def print_sorted_dictionary(a_dictionary):
     dicList = sorted(list(a_dictionary))
     for var in dicList:
-        print("{}".format(a_dictionary[var]))
+        print("{}: {}".format(var, a_dictionary[var]))
