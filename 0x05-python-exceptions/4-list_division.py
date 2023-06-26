4884c80ec7b892bf9ec41f969f789c5feec7a382
ambrose <kolus7381@gmail.com>
2023-06-26 20:47:11 +0100

    fixed indentation issues

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -5,7 +5,7 @@ def list_division(my_list_1, my_list_2, list_length):
     result = 0
     for i in range(list_length):
         try:
-           result = my_list_1[i] / my_list_2[i]
+            result = my_list_1[i] / my_list_2[i]
         except TypeError:
             print("wrong type")
             result = 0
