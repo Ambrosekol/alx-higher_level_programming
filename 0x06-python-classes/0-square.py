d7328adb89a847858f95ff0bc4feea15ad016a69
ambrose <kolus7381@gmail.com>
2023-06-27 17:32:34 +0100

    fixed pycodestyle errors

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -3,6 +3,8 @@
 This is a module that creates a Square class for ALX high level programming.
 This is task 0 and that is all this module will handle
 """
+
+
 class Square:
     """
     This creates a Square class and that is all this is supposed to do.
