84d3aa68ed8d89f4c2aecb43fbfd5e04061e0540
ambrose <kolus7381@gmail.com>
2023-06-27 20:52:52 +0100

    fixed logical error

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -49,7 +49,7 @@ class Square:
     def position(self, value):
         """Sets the attribue named Position"""
         if type(value) != tuple or len(value) != 2 or type(value[0]) != int \
-                or type(value[1]) != int or value[0] >= 0 or value[1] >= 0:
+                or type(value[1]) != int or value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
