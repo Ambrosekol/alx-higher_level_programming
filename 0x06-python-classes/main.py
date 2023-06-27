1eb233ef9664085e59999441a3424fda09c0124a
ambrose <kolus7381@gmail.com>
2023-06-27 20:54:20 +0100

    modified main

--- 0x06-python-classes/main.py
+++ 0x06-python-classes/main.py
@@ -2,12 +2,11 @@
 Square = __import__('6-square').Square
 
 my_square_1 = Square(3)
-my_square_1.size = 6
-print(my_square_1.size)
+my_square_1.my_print()
 
 print("--")
 
-my_square_2 = Square(3, (1, 1))
+my_square_2 = Square(3, (1, 5))
 my_square_2.my_print()
 
 print("--")
