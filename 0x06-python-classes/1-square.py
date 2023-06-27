662f8dedd37c4c6e6bbbf716108953f3124b6cc3
ambrose <kolus7381@gmail.com>
2023-06-27 17:42:46 +0100

    new square class with an Init method

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This is a module that creates a Square class with initialization of Square size
+for ALX high level programming.
+This is task 1 and that is all this module will handle
+"""
+
+
+class Square:
+    """
+    This now a Square class with the size initialization
+    and that is all this is supposed to do.
+    """
+    def __init__(self, size=0):
+        self.__size = size
