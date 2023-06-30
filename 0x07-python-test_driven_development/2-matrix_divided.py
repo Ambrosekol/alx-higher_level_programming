840ba51651bbe47c132274624dab274e5becfa00
ambrose <kolus7381@gmail.com>
2023-06-30 14:28:03 +0100

    fixed test issues and added doc note for function

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -2,25 +2,35 @@
 
 """
 This module divides a matrix (List of Lists) by a given number
-and then returns a new matrix (List of Lists) with the answers to
-the division
+and then returns a new matrix (List of Lists) with the answers
+to the division
 """
 
 
 def matrix_divided(matrix, div):
+    """
+    This function calculates the division of a matrix by div.
+    Div must be a number greater than 0 and must be an int
+    else an error is thrown
+
+    Matrix cannont be any other thing except an int
+    """
     verify_len = []
     newlist = list()
     index = 0
-    if matrix == None:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if matrix is None:
+        raise TypeError("matrix must be a matrix (list of lists) \
+of integers/floats")
     if type(div) != int:
         raise TypeError("div must be a number")
     elif div == 0:
         raise ZeroDivisionError("division by zero")
     elif type(matrix[0]) != list:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix (list of lists) \
+of integers/floats")
     elif len(matrix) < 1:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix (list of lists) \
+of integers/floats")
     else:
         for val in matrix:
             verify_len.append(len(val))
@@ -31,8 +41,9 @@ def matrix_divided(matrix, div):
                 newlist.append(list())
                 for val in arr:
                     if type(val) != int:
-                        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                        raise TypeError("matrix must be a matrix \
+(list of lists) of integers/floats")
                     else:
-                        newlist[index].append(val/div)
+                        newlist[index].append(round(val/div, 2))
                 index += 1
         return newlist
