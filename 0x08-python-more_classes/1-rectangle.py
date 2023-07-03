6134c4df2a845c1b72c89aa335230c8defdadded
ambrose <kolus7381@gmail.com>
2023-07-03 17:14:09 +0100

    added more item in class

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+This is a module containing the class for a rectangle.
+
+It contains nothing except pass
+"""
+
+
+class Rectangle:
+    """
+    This is an empty class containing nothing but the pass
+    command to enable it throw no error
+    """
+    
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+        
+    @property
+    def width(self):
+        return self.__width
+    
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+    
+    @property
+    def height(self):
+        return self.__height
+    
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
