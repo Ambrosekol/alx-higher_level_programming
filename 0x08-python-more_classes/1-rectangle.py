14f0f29c3be39067d3b73b39c4c0c38ea06c2b52
ambrose <kolus7381@gmail.com>
2023-07-03 17:50:17 +0100

    new comit

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -12,18 +12,15 @@ class Rectangle:
     """
 
     def __init__(self, width=0, height=0):
-        """This initializes the ability to give paramaters"""
         self.__width = width
         self.__height = height
 
     @property
     def width(self):
-        """This is the width getter"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """This is the width setter"""
         if type(value) != int or type(value) is None:
             raise TypeError("width must be an integer")
         elif value < 0:
@@ -33,12 +30,10 @@ class Rectangle:
 
     @property
     def height(self):
-        """This is the height getter"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """This is the height setter"""
         if type(value) != int or type(value) is None:
             raise TypeError("height must be an integer")
         elif value < 0:
