7ab1662eb57a7c0da5abc1fa4b274ce204e2ee3c
ambrose <kolus7381@gmail.com>
2023-07-03 22:31:53 +0100

    fixed more isfixed more issues

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -30,7 +30,7 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if type(value) != int or type(value) is None:
+        if type(value) != int:
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
@@ -43,7 +43,7 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        if type(value) != int or type(value) is None:
+        if type(value) != int:
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
@@ -51,11 +51,10 @@ class Rectangle:
             self.__height = value
 
     def area(self):
-        return self.__height * self.__width
+        return self.__width * self.__height
 
     def perimeter(self):
         if self.__height == 0 or self.__width == 0:
             return 0
-        else:
-            peri = 2 * (self.__width + self.__height)
-            return peri
+        peri = 2 * (self.__width + self.__height)
+        return peri
