5b591112adfe3c94df45330f7b31d71c19aff4cd
ambrose <kolus7381@gmail.com>
2023-07-10 21:10:39 +0100

    new commit

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+"""
+This is a module for printing out the the list of items in our basal
+class
+"""
+
+
+def lookup(obj):
+    """
+    Function that returns the names of attributes in a class
+    """
+    return dir(obj)
