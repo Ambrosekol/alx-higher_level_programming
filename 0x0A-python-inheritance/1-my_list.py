e5dffe4e0dace4d3903fe86591712eb95ac35dec
ambrose <kolus7381@gmail.com>
2023-07-10 22:21:52 +0100

    new commit

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module contains code of a class named MyList
+that inherits from list
+"""
+
+class MyList(list):
+    """
+    This is a class that has Public instance
+    method: def print_sorted(self): that prints the list,
+    but sorted (ascending sort)
+    """
+    def print_sorted(self):
+        srtlist = sorted(self)
+        print(srtlist)
