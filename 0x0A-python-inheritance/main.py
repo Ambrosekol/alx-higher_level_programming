f63915294e89b4ad0b1699b4ececba0d13b8fa2a
ambrose <kolus7381@gmail.com>
2023-07-10 22:41:21 +0100

    new

--- 0x0A-python-inheritance/main.py
+++ 0x0A-python-inheritance/main.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 MyList = __import__('1-my_list').MyList
 my_list = MyList()
-
-print(my_list(None))
+my_list.append('a')
+my_list.append('b')
+my_list.append(2)
+my_list.append(3)
+my_list.append(5)
+print(my_list)
+my_list.print_sorted()
+print(my_list)
