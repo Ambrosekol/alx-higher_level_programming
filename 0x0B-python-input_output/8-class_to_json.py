c9ae14187c5cd2a6400b9ccecf639b73b4eda39a
ambrose <kolus7381@gmail.com>
2023-07-11 18:58:08 +0100

    new commit

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -17,4 +17,4 @@ def class_to_json(obj):
     simple data structure (list, dictionary,string, integer
     and boolean) for JSON serialization of an object
     """
-    return json.loads(obj.dir())
+    return obj.__dict__
