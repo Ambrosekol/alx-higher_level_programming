9c5740bb10ce034aee8fb30bb695cea0dc8c5be9
ambrose <kolus7381@gmail.com>
2023-07-11 17:14:54 +0100

    new fix

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -6,9 +6,9 @@ JSON representation of an object (string)
 """
 
 
-def to_json_string(my_str):
+def to_json_string(my_obj):
     """
     this accepts an object and then the function
     returns the JSON representation of an object (string)
     """
-    return json.dump(my_str)
+    return json.dump(my_obj)
