150408d035235a00953be4fdbb1617434eede2e5
ambrose <kolus7381@gmail.com>
2023-07-11 17:24:09 +0100

    new commit

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-import json
+
 """
 This module contains all a a function that returns the
 JSON representation of an object (string)
 """
 
 
+import json
 def to_json_string(my_obj):
     """
     this accepts an object and then the function
