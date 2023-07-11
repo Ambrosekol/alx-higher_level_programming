618860b33881fd94814018c9f24e81b244bc2c77
ambrose <kolus7381@gmail.com>
2023-07-11 19:13:15 +0100

    fixed issues with pycodestyle

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -18,11 +18,8 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-    
 
     def to_json(self):
-        return {
-            "first_name": self.first_name,
-            "last_name": self.last_name,
-            "age": self.age
-        }
+        return {"first_name": self.first_name,
+                "last_name": self.last_name,
+                "age": self.age}
