9c667d51ca94966eeee8b2f8889efe6e4549c563
ambrose <kolus7381@gmail.com>
2023-07-11 15:38:53 +0100

    new commit

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+This file contains the module of a function that reads a
+text file (UTF8) and prints it to stdout
+"""
+
+
+def read_file(filename=""):
+    """
+    This function reads a file name and prints to stdout
+    """
+    with open(filename, encoding="UTF-8") as newfile:
+        newfile.read()
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,1 @@
+This is the directory containing modules for file navigation in Python
