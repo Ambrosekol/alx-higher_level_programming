7285aa51f0cfa1671c5b6844af45b498546ac818
ambrose <kolus7381@gmail.com>
2023-07-14 15:51:18 +0100

    fixed issues

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -96,5 +96,7 @@ class Rectangle(Base):
         return self.__height * self.__width
 
     def display(self):
+        for space in range(self.__y):
+            print()
         for i in range(self.__height):
-            print("#" * self.__width)
+            print(" " * self.__x + "#" * self.__width)
