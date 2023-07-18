86af71cb948fc413c1aea97e71633484b149f7c2
ambrose <kolus7381@gmail.com>
2023-07-18 21:56:43 +0100

    new

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-from models.base import Base
-
-
 """
 This file contains the class for the rectangle class
 """
+from models.base import Base
 
 
 class Rectangle(Base):
@@ -13,6 +11,21 @@ class Rectangle(Base):
     """
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize a new Rectangle.
+
+        Args:
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
+            x (int): The x coordinate of the new Rectangle.
+            y (int): The y coordinate of the new Rectangle.
+            id (int): The identity of the new Rectangle.
+        Raises:
+            TypeError: If either of width or height is not an int.
+            ValueError: If either of width or height <= 0.
+            TypeError: If either of x or y is not an int.
+            ValueError: If either of x or y < 0.
+        """
+
         if type(width) is not int:
             raise TypeError("width must be an integer")
         elif width <= 0:
