9c46249aba321a547476287fe24db462fbb74334
ambrose <kolus7381@gmail.com>
2023-09-13 01:10:32 +0100

    new

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,5 +2,5 @@
 if (isNaN(parseInt(process.argv[2]))) {
   console.log('Not a Number');
 } else {
-  console.log(parseInt('My number: ' + `${process.argv[2]}`));
+  console.log('My number: ' + parseInt(process.argv[2]));
 }
