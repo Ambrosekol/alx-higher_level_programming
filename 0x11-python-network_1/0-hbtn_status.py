77b9d0cc1ba75b12a8c5d3027680120571e40ef9
ambrose <kolus7381@gmail.com>
2023-10-01 11:04:54 +0100

    new

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 """
-This is a script that that fetches https://alx-intranet.hbtn.io/status
-You must use the package urllib
-You are not allowed to import any packages other than urllib
-The body of the response must be displayed like
-    the following example (tabulation before -)
-You must use a with statement
+This script fetches data from
+    https://alx-intranet.hbtn.io/status
 """
+
+
 import urllib.request
 
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as r:
-    response = r.read()
-    print("\t - type: {}".format(type(response)))
-    print("\t - content: {}".format(response))
-    print("\t - utf8 content: {}".format(response.decode('utf-8')))
+with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+    body = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode('utf-8')))
